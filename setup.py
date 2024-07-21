5b46bd17103d8940df73be425fc89a8df5768b87
Hugo Rameil <hugorameil@gmail.com>
2024-07-21 21:32:26 +0200

    update

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ramo',
+    version='1.0.0',
+    author='hugo rameil',
+    author_email='hugorameil@gmail.com',
+    url=,
+    description='homemade package for gain in productivity',
+    packages=find_packages(),
+    readme="",
+    install_requires=,
+     
+    
+)
